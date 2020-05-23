1b2e5cd4f7f190d4fbf95e777b65d12901acf476
Edouard-Bonnard <edouard.bonnard@gmail.com>
2020-05-23 07:43:09 +0200

    working MNIST SKlearn

--- OC_k-NN.py
+++ OC_k-NN.py
@@ -4,6 +4,7 @@
 # coding UTF-8
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.datasets import fetch_openml
 mnist = fetch_openml('mnist_784', version=1)
@@ -20,12 +21,23 @@ xtrain, xtest, ytrain, ytest = train_test_split(data, target, train_size=0.8)
 #k-NN
 from sklearn import neighbors
 
-#Entrainement
+#Entrainement pour k=3
 knn = neighbors.KNeighborsClassifier(n_neighbors=3)
 knn.fit(xtrain, ytrain)
-
 #Erreur du classifieur
 error = 1 - knn.score(xtest, ytest)
-print('Erreur: %f' % error)
 
-print(mnist.data.shape)
+#Entrainement pour k de 2 à 14
+errors=[]
+for k in range(2,15):
+    knn = neighbors.KNeighborsClassifier(k)
+    knn.fit(xtrain, ytrain)
+    errors.append(1-knn.score(xtest, ytest))
+
+plt.plot(range(2,15),errors,'-o')
+plt.show()
+
+
+
+
+
